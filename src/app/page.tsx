6b9cdd395f5edf18a67071ce30d9846ebd25d064
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-07-09 01:31:04 +0800

    whole site lmao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,77 @@
-import Image from "next/image";
+import { FaRunning, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import SignUp from "./signUpCard";
+import Image from 'next/image';
+import RunClubLogo from './../images/runclubimage.svg';
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center min-h-screen relative">
+      <BackgroundBeams />
+
+      {/* text and image */}
+      <div className="flex flex-row items-center space-x-12 z-99">
+
+        {/* text containers */}
+        <div className="flex flex-col space-y-8">
+
+          {/* app name and logo */}
+          <div className="flex flex-row items-start text-gray-300">
+            <h2 className="font-regular text-3xl tracking-tighter">Runner's High</h2>
+            <FaRunning />
+          </div>
+          {/* main text */}
+          <div>
+            <h1 className="text-8xl font-medium text-black tracking-tighter">Find</h1>
+            <h1 className="text-8xl font-medium text-black tracking-tighter">Run</h1>
+            <h1 className="text-8xl font-medium text-black tracking-tighter">Clubs</h1>
+            <h1 className="text-8xl font-bold text-white tracking-tighter rainbow-text">with Vibez
+            </h1>
+          </div>
+          {/* subtext */}
+          <div className="flex flex-row items-start opacity-30">
+            <h2 className="font-regular text-3xl tracking-tighter">Built By Runners For Runners</h2>
+          </div>
+
+          <div data-tf-live="01J29C5FPP98PYMRZRE7N3G3W5"></div><script src="//embed.typeform.com/next/embed.js"></script>
+
+        </div>
+
+        {/* image with glow */}
+        <div className="relative">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="size-[600px] bg-white rounded-full opacity-90 blur-4xl"></div>
+          </div>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="w-96 h-96 bg-[#EB5147] rounded-full opacity-90 blur-3xl"></div>
+          </div>
+          <Image src={RunClubLogo} alt="Run Club Logo" width={850} height={850} className="relative z-10" />
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* <SignUp /> */}
+
+      <div className="absolute bottom-0 w-4/5 bg-transparent text-white py-4 border-t border-black border-opacity-30 px-20 mx-20">
+        <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+          <div className="text-center md:text-left">
+            <p className="text-sm">© 2024 Runner's High. All rights reserved.</p>
+            <p className="text-sm">Terms | Privacy | Security</p>
+          </div>
+          <div className="flex space-x-4 mt-2 md:mt-0">
+            <a href="https://www.facebook.com" className="text-white hover:text-gray-400">
+              <FaFacebook size={24} />
+            </a>
+            <a href="https://www.twitter.com" className="text-white hover:text-gray-400">
+              <FaTwitter size={24} />
+            </a>
+            <a href="https://www.instagram.com" className="text-white hover:text-gray-400">
+              <FaInstagram size={24} />
+            </a>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
---
+++ src/app/signUpCard.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function SignUp() {
+  return (
+    <main className="h-[700px]">
+        <div className="w-screen p-24 ">
+        <div data-tf-live="01J29C5FPP98PYMRZRE7N3G3W5"></div><script src="//embed.typeform.com/next/embed.js"></script>
+      </div>
+
+    </main>
+  );
+}
