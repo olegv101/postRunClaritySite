c90ebec7825afafc43d76e1103215278483328b3
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-07-16 12:56:22 +0800

    added bento

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -8,61 +8,63 @@ export default function Features() {
             title: "Built for developers",
             description:
                 "Built for engineers, developers, dreamers, thinkers and doers.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "Ease of use",
             description:
                 "It's as easy as using an Apple, and as expensive as buying one.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "Pricing like no other",
             description:
                 "Our prices are best in the market. No cap, no lock, no credit card required.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "100% Uptime guarantee",
             description: "We just cannot be taken down by anyone.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "Multi-tenant Architecture",
             description: "You can simply share passwords instead of buying new seats",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
-            title: "24/7 Customer Support",
+            title: "24/7 Support",
             description:
                 "We are available a 100% of the time. Atleast our AI Agents are.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "Money back guarantee",
             description:
                 "If you donot like EveryAI, we will convince you to like us.",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
         {
             title: "And everything else",
             description: "I just ran out of copy ideas. Accept my sincere apologies",
-            icon: <RiRunFill />,
+            icon: <RiRunFill size={32}/>,
         },
     ];
 
     return (
-        <div className='min-h-screen p-20 flex justify-center'>
-            <div className="flex flex-col">
-                <h1 className='text-9xl'>Features</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-6xl">
-                    {features.map((feature, index) => (
-                        <div className='w-60 p-10 rounded-lg bg-pink-400 text-white' key={index}>
-                            <h2 className='text'>{feature.title}</h2>
-                            {feature.icon}
-                            <p>{feature.description}</p>
-                        </div>
-                    ))}
+        <div className='bg-grid-black/[0.2]'>
+            <div className='min-h-screen p-20 flex justify-center '>
+                <div className="flex flex-col">
+                    <h1 className='text-9xl font-semibold mx-auto mb-10'>Features</h1>
+                    <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full max-w-6xl">
+                        {features.map((feature, index) => (
+                            <div className='hover:-translate-y-2 hover:border hover:border-black transition ; w-60 p-10 rounded-lg bg-pink-400 text-white' key={index}>
+                                <div className='mb-2'>{feature.icon}</div>
+                                <h2 className='text-2xl font-semibold mb-2'>{feature.title}</h2>
+                                <p className='opacity-80'>{feature.description}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
